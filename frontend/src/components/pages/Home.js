7703d1980623dec54b23cc74205ac02c8c9b1371
Gerson107 <danimejia1995@hotmail.com>
2022-02-07 14:06:54 -0500

    actualizado

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -3,14 +3,13 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-import NosSection from '../NosSection';
+
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
-      <NosSection/>
       <Footer />
     </>
   );
